24a5d1abe5aad1ff7b9f0d8bcf4f82c426912769
dev-seth <seth.tba@gmail.com>
2016-02-29 21:23:42 +0200

    gunicorn

--- init.sh
+++
@@ -1,7 +0,0 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx-s.conf
-sudo /etc/init.d/nginx restart
-
-sudo rm /etc/gunicorn.d/*
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart

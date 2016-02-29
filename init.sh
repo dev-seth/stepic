2fbc24896b8ea8c0aff343ee7fecfca6bae49078
dev-seth <seth.tba@gmail.com>
2016-02-29 19:42:52 +0200

    gunicorn added

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-# sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+
+sudo rm /etc/gunicorn.d/django.example
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart

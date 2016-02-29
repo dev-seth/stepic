7699a7581ffe7e025d4cce58d9ed2dc734be3b70
dev-seth <seth.tba@gmail.com>
2016-02-29 21:29:06 +0200

    cleaning

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/nginx-s.conf
+sudo /etc/init.d/nginx restart
+
+sudo rm /etc/gunicorn.d/*
+sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart

3434a86239cc7069ba6acc2689aca6aa650f7c01
dev-seth <seth.tba@gmail.com>
2016-03-01 19:09:14 +0200

    gunicorn prefinal

--- web/hello.py
+++ web/hello.py
@@ -2,5 +2,5 @@ def app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-#    return [environ['QUERY_STRING'].replace('&', '\n')]
-    return ['a', 'b', 'c', 'd']
+    return [environ['QUERY_STRING'].replace('&', '\n')]
+#    return ['1', '2', '3', '5', '6']

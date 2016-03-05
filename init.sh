76d139a49a61e69f0f4852fa467f5e9ad25cf1d6
dev-seth <seth.tba@gmail.com>
2016-03-05 20:47:03 +0200

    django 2

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ sudo /etc/init.d/nginx restart
 
 sudo rm /etc/gunicorn.d/*
 sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/etc/ask.conf /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart

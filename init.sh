b5ab96a7497c84f22c570a842cbc28e21ec63c8b
dev-seth <seth.tba@gmail.com>
2016-03-06 10:14:52 +0200

    ssh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo pip install django==1.7.7                                                          
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/nginx-s.conf
 sudo /etc/init.d/nginx restart
@@ -6,4 +8,3 @@ sudo rm /etc/gunicorn.d/*
 sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/etc/ask.conf /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
-pip install django==1.7.7                                                          
